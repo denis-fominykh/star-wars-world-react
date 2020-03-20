10f679bfad7ce02ab34cd8059085000db838af66
denis-fominykh <fominykhdenis@gmail.com>
2020-03-20 19:13:34 +0200

    starting a project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './App.scss';
+
+export const App = () => {
+  return (
+    <React.Fragment>
+      <h1>The Star Wars World</h1>
+    </React.Fragment>
+  );
+}
