47ecd937e3a0d123e7d555d76940117eae609f79
denis-fominykh <fominykhdenis@gmail.com>
2020-03-21 21:43:19 +0200

    creating a random planet component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './App.scss';
 
 import { Header } from './components/Header/Header';
+import { RandomPlanet } from './components/RandomPlanet/RandomPlanet';
 
 export const App = () => {
   return (
     <div>
       <Header />
+      <RandomPlanet />
     </div>
   );
 }
