09506d93f1bb008ea3ed1b6de097b07f3d34bf69
denis-fominykh <fominykhdenis@gmail.com>
2020-03-21 21:51:51 +0200

    creating the item list component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import './App.scss';
 
 import { Header } from './components/Header/Header';
 import { RandomPlanet } from './components/RandomPlanet/RandomPlanet';
+import { ItemList } from './components/ItemList/ItemList';
 
 export const App = () => {
   return (
     <div>
       <Header />
       <RandomPlanet />
+      <div className="row mb2">
+        <div className="col-md-6">
+          <ItemList />
+        </div>
+      </div>
     </div>
   );
 }
