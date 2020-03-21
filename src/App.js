3701503e7bb500940f868fcee2d96b193f369948
denis-fominykh <fominykhdenis@gmail.com>
2020-03-21 22:00:18 +0200

    creating the person details component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.scss';
 import { Header } from './components/Header/Header';
 import { RandomPlanet } from './components/RandomPlanet/RandomPlanet';
 import { ItemList } from './components/ItemList/ItemList';
+import { PersonDetails } from './components/PersonDetails/PersonDetails';
 
 export const App = () => {
   return (
@@ -14,6 +15,9 @@ export const App = () => {
       <div className="row mb2">
         <div className="col-md-6">
           <ItemList />
+        </div>
+        <div className="col-md-6">
+          <PersonDetails />
         </div>
       </div>
     </div>
