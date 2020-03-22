04ec7c12858997c333042fc3073aaa89718ac011
denis-fominykh <fominykhdenis@gmail.com>
2020-03-22 16:56:11 +0200

    creating the methods for transform the data API

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -9,37 +9,31 @@ export class RandomPlanet extends Component {
     super(props);
     this.swapiService = new SwapiService();
     this.state = {
-      id: null,
-      name: null,
-      population: null,
-      rotationPeriod: null,
-      diameter: null,
+      planet: {},
     };
     this.updatePlanet();
+  }
+
+  onPlanetLoaded = (planet) => {
+    this.setState({ planet });
   }
 
   updatePlanet() {
     const id = Math.floor(Math.random() * 25) + 2;
     this.swapiService
       .getPlanet(id)
-      .then(planet => {
-        this.setState({
-          id: id,
-          name: planet.name,
-          population: planet.population,
-          rotationPeriod: planet.rotation_period,
-          diameter: planet.diameter,
-        });
-      })
+      .then(this.onPlanetLoaded)
   }
 
   render() {
     const {
-      id,
-      name,
-      population,
-      rotationPeriod,
-      diameter
+      planet: {
+        id,
+        name,
+        population,
+        rotationPeriod,
+        diameter
+      }
     } = this.state;
 
     return (
