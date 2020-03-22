a8efc39dd6caa49a77c335e65c3fd9f7b836b3d9
denis-fominykh <fominykhdenis@gmail.com>
2020-03-22 15:31:06 +0200

    fixing the styles and the missed colons

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -52,15 +52,15 @@ export class RandomPlanet extends Component {
           <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
-              <span className="term">Population</span>
+              <span className="term">Population:</span>
               <span>{population}</span>
             </li>
             <li className="list-group-item">
-              <span className="term">Rotation Period</span>
+              <span className="term">Rotation Period:</span>
               <span>{rotationPeriod}</span>
             </li>
             <li className="list-group-item">
-              <span className="term">Diameter</span>
+              <span className="term">Diameter:</span>
               <span>{diameter}</span>
             </li>
           </ul>
