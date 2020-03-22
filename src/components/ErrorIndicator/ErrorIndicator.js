7b3fe0dd04e945dae3a2c769281fad0ac8b9c404
denis-fominykh <fominykhdenis@gmail.com>
2020-03-22 21:12:41 +0200

    adding the icon to the error indicator

--- src/components/ErrorIndicator/ErrorIndicator.js
+++ src/components/ErrorIndicator/ErrorIndicator.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import './ErrorIndicator.scss';
+import deathStarIcon from './death-star.png';
 
 export const ErrorIndicator = () => {
   return (
     <div className="error-indicator">
+      <img className="error-icon" src={deathStarIcon} alt="error icon"/>
       <span className="boom">BOOM!</span>
       <span>
         something has gone terribly wrong
