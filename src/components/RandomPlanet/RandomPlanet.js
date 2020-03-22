38ee19a3a9fd69e2c301806a25ed3543289b730a
denis-fominykh <fominykhdenis@gmail.com>
2020-03-22 21:03:25 +0200

    adding the error indicator to the project

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -5,6 +5,7 @@ import './RandomPlanet.scss';
 import { SwapiService } from '../../services/swapiService';
 import { Spinner } from '../Spinner/Spinner';
 import { PlanetView } from '../PlanetView/PlanetView';
+import { ErrorIndicator } from '../ErrorIndicator/ErrorIndicator';
 
 export class RandomPlanet extends Component {
   constructor(props) {
@@ -13,6 +14,7 @@ export class RandomPlanet extends Component {
     this.state = {
       planet: {},
       loading: true,
+      error: false,
     };
     this.updatePlanet();
   }
@@ -24,21 +26,31 @@ export class RandomPlanet extends Component {
     });
   }
 
+  onError = error => {
+    this.setState({
+      loading: false,
+      error: true,
+    })
+  }
+
   updatePlanet() {
     const id = Math.floor(Math.random() * 25) + 2;
     this.swapiService
       .getPlanet(id)
       .then(this.onPlanetLoaded)
+      .catch(this.onError);
   }
 
   render() {
-    const { planet, loading } = this.state;
+    const { planet, loading, error } = this.state;
 
+    const errorIndicator = error ? <ErrorIndicator /> : null;
     const spinner = loading ? <Spinner /> : null;
-    const content = !loading ? <PlanetView planet={planet}/> : null;
+    const content = !(loading || error) ? <PlanetView planet={planet}/> : null;
 
     return (
       <div className="random-planet jumbotron rounded">
+        {errorIndicator}
         {spinner}
         {content}
       </div>
