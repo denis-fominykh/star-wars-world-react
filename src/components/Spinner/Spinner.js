5c40e4a607e4ef47dcae13ed80c9614b86494fec
denis-fominykh <fominykhdenis@gmail.com>
2020-03-22 18:20:09 +0200

    fixing the class attributes

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -4,8 +4,8 @@ import './Spinner.scss';
 
 export const Spinner = () => {
   return (
-    <div class="loadingio-spinner-double-ring-p9rapysvl5">
-      <div class="ldio-9o98h6jgbfw">
+    <div className="loadingio-spinner-double-ring-p9rapysvl5">
+      <div className="ldio-9o98h6jgbfw">
         <div></div>
         <div></div>
         <div>
