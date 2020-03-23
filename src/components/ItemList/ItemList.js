29260d863fe25949a85bae7437cb616ed163026a
denis-fominykh <fominykhdenis@gmail.com>
2020-03-23 15:19:08 +0200

    displaying the list of persons

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,20 +1,56 @@
 import React, { Component } from 'react';
 
 import './ItemList.scss';
+import { SwapiService } from '../../services/swapiService';
+import { Spinner } from '../Spinner/Spinner';
 
 export class ItemList extends Component {
+  swapiService = new SwapiService();
+
+  state = {
+    peopleList: null,
+  };
+
+  componentDidMount() {
+    this.swapiService
+      .getAllPeople()
+      .then(this.onPeopleLoaded)
+      .catch(this.onError);
+  }
+
+  onPeopleLoaded = peopleList => {
+    this.setState({ peopleList });
+  }
+
+  onError = error => {
+    this.setState({ error });
+  }
+
+  renderItems(arr) {
+    return arr.map(({id, name}) => {
+      return (
+        <li className="list-group-item"
+            key={id}
+            onClick={() => this.props.onItemSelected(id)}
+        >
+          {name}
+        </li>
+      );
+    })
+  }
+
   render() {
+    const { peopleList } = this.state;
+
+    if (!peopleList) {
+      return <Spinner />;
+    }
+
+    const items = this.renderItems(peopleList);
+
     return (
       <ul className="item-list list-group">
-        <li className="list-group-item">
-          Luke Skywalker
-        </li>
-        <li className="list-group-item">
-          Darth Vader
-        </li>
-        <li className="list-group-item">
-          R2-D2
-        </li>
+        {items}
       </ul>
     );
   }
