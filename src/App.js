f6d11971892eda4a5b70a3b03b551f40bdafcfbd
denis-fominykh <fominykhdenis@gmail.com>
2020-03-23 18:28:25 +0200

    displaying the person info

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.scss';
 
@@ -7,19 +7,31 @@ import { RandomPlanet } from './components/RandomPlanet/RandomPlanet';
 import { ItemList } from './components/ItemList/ItemList';
 import { PersonDetails } from './components/PersonDetails/PersonDetails';
 
-export const App = () => {
-  return (
-    <div className="container">
-      <Header />
-      <RandomPlanet />
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails />
+export class App extends Component {
+  state = {
+    selectedPerson: null,
+  };
+
+  onPersonSelected = (id) => {
+    this.setState({
+      selectedPerson: id,
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header />
+        <RandomPlanet />
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList onItemSelected={this.onPersonSelected}/>
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson}/>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
