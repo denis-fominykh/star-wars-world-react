5c4e024a20709a692a77c9dd424e2553b4b4edab
denis-fominykh <fominykhdenis@gmail.com>
2020-06-21 23:57:33 +0300

    fixing a tab size

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-	env: {
-		browser: true,
-		es6: true,
-	},
-	extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
-	globals: {
-		Atomics: 'readonly',
-		SharedArrayBuffer: 'readonly',
-	},
-	parser: 'babel-eslint',
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 2018,
-		sourceType: 'module',
-	},
-	plugins: ['react'],
-	rules: {},
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
 };
